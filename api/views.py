22983fdd64b0cff90d316f0f11d9d4c535d83531
RasheedAlwahbanyMinsati <rasheed.alwahbany@minsati.com>
2022-11-03 11:54:22 +0300

    Rest api using Serializer

--- api/views.py
+++ api/views.py
@@ -2,13 +2,15 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions
 from app.models import Item
+from api.items_serializer import ItemSerializer
+from rest_framework.renderers import JSONRenderer
+
 
 
 @api_view(['GET'])
 @permission_classes((permissions.AllowAny,))
 def getData(request):
-    items = Item.objects.all()
-    items_d=[]
-    for item in items:
-        items_d=items_d+["name:"+item.name]
-    return Response(items_d)
+    items = ItemSerializer( Item.objects.all(),many=True)
+    # json = JSONRenderer().render(items.data)
+
+    return Response(items.data)
